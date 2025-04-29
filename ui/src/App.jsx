98967fd737de6aa4d68be371b0dc416d84c3deee
penszzip <pazhiloyflex@gmail.com>
2025-04-29 16:27:38 -0400

    establish backend and frontend connection

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [projects, setProjects] = useState([])
 
-  
+  useEffect(() => {
+    axios.get('http://localhost:8080/projects')
+          .then(response => {
+            console.log(response)
+            setProjects(response.data)
+          })
+  }, [])
 
   return (
     <>
-      
+      {projects.map(project => <h1>{project.name}</h1>)}
     </>
   )
 }
