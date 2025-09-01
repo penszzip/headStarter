69721208f2f1f2598bc2b0cf679e6415b1ddbc9f
penszzip <pazhiloyflex@gmail.com>
2025-09-01 19:06:54 -0400

    Filter chain config

--- src/main/java/com/penszzip/headStarter/config/SecurityConfig.java
+++ src/main/java/com/penszzip/headStarter/config/SecurityConfig.java
@@ -1,7 +1,21 @@
 package com.penszzip.headStarter.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+import com.penszzip.headStarter.jwt.JwtAuthenticationFilter;
 
 import lombok.AllArgsConstructor;
 
@@ -9,5 +23,45 @@ import lombok.AllArgsConstructor;
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig {
+    private JwtAuthenticationFilter jwtAuthFilter;
+    private UserDetailsService userDetailsService;
     
+    // Configure HTTP security filter chain
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(
+                auth -> auth.requestMatchers("/auth/**").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/projects/**").permitAll()
+                            .anyRequest().authenticated())
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            // Add custom JWT filter before any other spring's authentication
+            .addFilterAfter(jwtAuthFilter, LogoutFilter.class);
+        
+            return http.build();
+    }
+
+    // Password encoder for authentication provider
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Configure authentication provider
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    // Configure authentication manager
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
